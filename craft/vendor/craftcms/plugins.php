66e17340134d00b3c59d502d286a9e6db325283a
Kelly <kellyannmcnamara01@gmail.com>
2019-07-23 22:22:46 -0400

    index, footer, craft update, redactor plugin, typed link plugin

---
+++ craft/vendor/craftcms/plugins.php
@@ -0,0 +1,39 @@
+<?php
+
+$vendorDir = dirname(__DIR__);
+$rootDir = dirname(dirname(__DIR__));
+
+return array (
+  'sebastianlenz/linkfield' => 
+  array (
+    'class' => 'typedlinkfield\\Plugin',
+    'basePath' => $vendorDir . '/sebastianlenz/linkfield/src',
+    'handle' => 'typedlinkfield',
+    'aliases' => 
+    array (
+      '@typedlinkfield' => $vendorDir . '/sebastianlenz/linkfield/src',
+    ),
+    'name' => 'Typed link field',
+    'version' => '1.0.19',
+    'description' => 'A Craft field type for selecting links',
+    'developer' => 'Sebastian Lenz',
+    'developerUrl' => 'https://github.com/sebastian-lenz/',
+  ),
+  'craftcms/redactor' => 
+  array (
+    'class' => 'craft\\redactor\\Plugin',
+    'basePath' => $vendorDir . '/craftcms/redactor/src',
+    'handle' => 'redactor',
+    'aliases' => 
+    array (
+      '@craft/redactor' => $vendorDir . '/craftcms/redactor/src',
+    ),
+    'name' => 'Redactor',
+    'version' => '2.3.3.2',
+    'description' => 'Edit rich text content in Craft CMS using Redactor by Imperavi.',
+    'developer' => 'Pixel & Tonic',
+    'developerUrl' => 'https://pixelandtonic.com/',
+    'developerEmail' => 'support@craftcms.com',
+    'documentationUrl' => 'https://github.com/craftcms/redactor/blob/v2/README.md',
+  ),
+);
